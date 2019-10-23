b376d8cf8b9dd4d0a2e603cd73817f11cb522a09
MANISH50 <manishkumar.ranchi007@gmail.com>
2019-10-23 22:59:18 +0530

    Update WeightString.java

--- LBH1/WeightString.java
+++ LBH1/WeightString.java
@@ -27,26 +27,22 @@ public class WeightString {
   private static int getWeight(String input1,int input2){
 	  int count= 0;
 	  String vowels= "aAeEiIoOuU";
-	  if(input2 == 1){
+	  for(int i=0;i<input1.length();i++){
 		  
-		  for(int i=0;i<input1.length();i++){
-			  
-			   if(Character.isAlphabetic(input1.charAt(i))){
-				   count=count+getCount( input1.charAt(i) );
-			   }
-		  }
-		  
-	  }else{
-		  
-		  for(int i=0;i<input1.length();i++){
-			  
-			   if( Character.isAlphabetic(  input1.charAt(i)) && !vowels.contains( String.valueOf(input1.charAt(i)) ) ){
-				   count=count+getCount(input1.charAt(i));
-			   }
-		  }
-		  
+		   if(Character.isAlphabetic(input1.charAt(i))){
+			   
+			      if(input2== 1){
+			    	  count+=getCount( input1.charAt(i) );
+			      }else{
+			    	  
+			    	   if(  !vowels.contains( String.valueOf(  input1.charAt(i)  ) )  ){
+			    		   count+=getCount( input1.charAt(i) );
+			    	   }
+			      }
+			   
+		   }
+		   
 	  }
-	  
 	  return count;
   }
   
